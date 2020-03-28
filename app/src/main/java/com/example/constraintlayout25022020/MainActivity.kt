9e304fcb7505b532270ea95f9ab511bc8c50e3bd
phuc199718 <phuc199718@gmail.com>
2020-03-28 12:50:51 +0700

    linearlayout bai so 1

---
+++ app/src/main/java/com/example/constraintlayout25022020/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintlayout25022020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
